3ccc31d8ca9080b5aa61b9907fce2990bc6ec8c8
Pangsua Vang <pangsua26@yahoo.com>
2020-05-01 15:28:07 -0500

    coded ready function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Work hour varibles
 var workDay = {
     "9AM": "",
     "10AM": "",
@@ -9,3 +10,14 @@ var workDay = {
     "4PM": "",
     "5PM": "",
 };
+
+// Function to run page and store workDay when ready/updated
+$(document).ready(function(){
+    if(!localStorage.getItem("workDay")) {
+      updateCalendar(workDay);
+    } else {
+      updateCalendar(JSON.parse(localStorage.getItem("workDay")));
+    }
+  })
+
+
