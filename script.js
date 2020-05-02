3b8817f3599cf24f805ad1068def10d55d13083b
Pangsua Vang <pangsua26@yahoo.com>
2020-05-02 16:35:46 -0500

    added block code for current/past/future/ - added css

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-// Work hour varibles
+// Work day hours varibles
 var workDayHours = {
-    "9AM": "",
-    "10AM": "",
-    "11AM": "",
-    "12PM": "",
-    "1PMM": "",
-    "2PM": "",
-    "3PM": "",
-    "4PM": "",
-    "5PM": "",
+  "9AM": "",
+  "10AM": "",
+  "11AM": "",
+  "12PM": "",
+  "1PM": "",
+  "2PM": "",
+  "3PM": "",
+  "4PM": "",
+  "5PM": "",
 };
 
-// Function to run page and store workDay when ready/updated
+// Function to run page and store work day hours when ready/updated
 $(document).ready(function(){
-    if(!localStorage.getItem("workDay")) {
-      updateCalendar(workDayHours);
-    } else {
-      updateCalendar(JSON.parse(localStorage.getItem("workDay")));
-    }
-  })
+  if(!localStorage.getItem("workDayHours")) {
+    updateCalendarTasks(workDayHours);
+  } else {
+    updateCalendarTasks(JSON.parse(localStorage.getItem("workDayHours")));
+  }
+})
 
 // Code to capture today's date
 $('#date-today h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do'));
 
+let counter = 1;
+for(const property in workDayHours) {
+  var task = "#task" + counter;
+  $(task).text(workDayHours[property]);
+  var timeId = "#time" + counter;
+  var currentHour = moment().hour();
+  var timeString = $(timeId).text();
+  var timeNumber = hourNumberFromHourString(timeString);  
+  if(timeNumber < currentHour) {
+    $(task).addClass("past");
+  } else if (timeNumber > currentHour) {
+    $(task).addClass("future");
+  } else {
+    $(task).addClass("present");
+  }
+  counter ++;
+}
